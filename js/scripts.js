7cf89fd30910fb8b61019c6a83b0ad51f0c8d142
Zarina Mazieva <zmazieva78@gmail.com>
2019-11-08 13:58:52 -0800

    add prototype for whole pizza

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,54 @@ function PizzaSize(name, price) {
     this.price = price
 }
 
-function PizzaToppings(name, price) {
+function PizzaTopping(name, price) {
     this.name = name,
     this.price = price
 }
-
-
-function createAllPizzaSizes() {
-    var allSizes = [
-        new PizzaSize("small", 1.99),
-        new PizzaSize("medium", 2.99),
-        new PizzaSize("large", 3.99)
+var allSizes = [
+    new PizzaSize("small", 5.99),
+    new PizzaSize("medium", 6.99),
+    new PizzaSize("large", 8.99)
     ];
+var allToppings = [
+    new PizzaTopping("parmesan", 0.99),
+    new PizzaTopping("mozzarella", 0.99),
+    new PizzaTopping("pepperoni", 1.99),
+    new PizzaTopping("sausage", 1.99),
+    new PizzaTopping("tomatoes", 0.99),
+    new PizzaTopping("pepers", 0.99),
+    new PizzaTopping("onions", 0.99),
+    new PizzaTopping("mushrooms", 1.99),
+    new PizzaTopping("olives", 0.99),
+    new PizzaTopping("spinach", 0.99),
+];
 
-    return allSizes;
+function Pizza(size, toppings) {
+    this.size = size,
+    this.toppings = toppings
 }
- 
+
+Pizza.prototype.calculate = function() {
+    var sum = this.size.price;
+    this.toppings.forEach(topping => {
+        sum += topping.price;
+    });
+    return sum
+}
+$(function() {
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
